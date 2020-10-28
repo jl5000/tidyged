c0f98ebf0c9d893f24a7b99bda1a10c5ae0b1927
Jamie Lendrum <26303390+jl5000@users.noreply.github.com>
2020-10-28 19:14:52 +0000

    Update params file

--- R/params.R
+++ R/params.R
@@ -1,6 +1,5 @@
 
 
-
 #' @param address_city The name of the city/town used in the address. Isolated for sorting or indexing.
 #' @param address_country The name of the country that pertains to the associated address. Isolated by some systems for 
 #' sorting or indexing. Used in most cases to facilitate automatic sorting of mail.
@@ -8,15 +7,14 @@
 #' @param address_fax A FAX telephone number appropriate for sending data facsimiles.
 #' @param address_postal_code The ZIP or postal code used by the various localities in handling of mail. 
 #' Isolated for sorting or indexing.
-#' @param address_state The name of the US state/UK county used in the address. Isolated for sorting or indexing.
+#' @param address_state The name of the province, state or similar country subdivision used in the address. 
+#' Isolated for sorting or indexing.
 #' @param address_web_page The world wide web page address.
-#' @param adopted_by_which_parent A code which shows which parent in the associated family record adopted this person.
+#' @param adopted_by_which_parent A code which shows which parent in the associated family group record adopted this person.
 #' Use "HUSB" for husband, "WIFE" for wife, or "BOTH" for both.
 #' @param age_at_event,husband_age_at_event,wife_age_at_event A string that indicates the age in years, months, and days that the 
 #' principal was at the time of the associated event. Any labels must come after their 
 #' corresponding number, for example; 4y 8m 10d.
-#' @param ancestral_file_number A unique permanent record number of an individual record 
-#' contained in the Family History Department's Ancestral File.
 #' @param attribute_descriptor Text describing a particular characteristic or attribute assigned to an individual. 
 #' @param attribute_type An attribute which may have caused name, addresses, phone numbers, 
 #' family listings to be recorded. Its application is in helping to classify sources used for information.
@@ -37,19 +35,13 @@
 #' 2 = Secondary evidence, data officially recorded sometime after event
 #' 3 = Direct and primary evidence used, or by dominance of the evidence
 #' @param character_encoding A code value that represents the character set to be used to 
-#' interpret this data. Currently, the preferred character set is ANSEL, which 
-#' includes ASCII as a subset. UNICODE is not widely supported by most operating systems; 
-#' therefore, GEDCOM produced using the UNICODE character set will be limited in its 
-#' interchangeability for a while but should eventually provide the international flexibility 
-#' that is desired. Allowed values: "ANSEL", "UTF-8", "UNICODE", "ASCII"
+#' interpret this data. Allowed values: "UTF-8" and "UNICODE" (UTF-16).
 #' @param copyright_gedcom_file A copyright statement needed to protect the copyrights of the 
 #' submitter of this GEDCOM file.
 #' @param copyright_source_data A copyright statement required by the owner of data from 
-#' which this information was downloaded. For example, when a GEDCOM down-load is requested 
-#' from the Ancestral File, this would be the copyright statement to indicate that the data 
-#' came from a copyrighted source.
+#' which this information was downloaded.
 #' @param count_of_children The known number of children of this individual from all marriages or, 
-#' if subordinate to a family record, the reported number of children known to belong to this 
+#' if subordinate to a family group record, the reported number of children known to belong to this 
 #' family, regardless of whether the associated children are represented in the corresponding 
 #' structure. This is not necessarily the count of children listed in a family structure.
 #' @param count_of_marriages The number of different families that this person was known to have 
@@ -77,28 +69,20 @@
 #' @param event_type_individual A code used to indicate the type of individual event. 
 #' @param events_recorded An enumeration of the different kinds of events that were recorded in a 
 #' particular source. Each enumeration is separated by a comma. Such as a parish register of births, 
-#' deaths, and marriages would be BIRT, DEAT, MARR. These can be enumerated over more than one
-#' vector element. 
+#' deaths, and marriages would be BIRT, DEAT, MARR.
 #' @param file_creation_date A date_exact() object giving the date that this file was created. 
 #' @param gedcom_content_description A note that a user enters to describe the contents of the 
 #' lineage-linked file in terms of "ancestors or descendants of" so that the person receiving 
 #' the data knows what genealogical information the file contains. 
-#' @param gedcom_file_name The name of the GEDCOM file. If the file name includes a file 
-#' extension it must be shown in the form (filename.ext).
-#' @param gedcom_form The GEDCOM form used to construct this file. There maybe other 
-#' forms used such as CommSoft's "EVENT_LINEAGE_LINKED" but these specifications define only 
-#' the LINEAGE-LINKED Form. Systems will use this value to specify GEDCOM compatible with these specifications. 
-#' @param generations_of_ancestors The number of generations of ancestors included in this file. 
-#' This value is usually provided when FamilySearch programs build a GEDCOM file for a patron requesting 
-#' a download of ancestors.
-#' @param generations_of_descendants The number of generations of descendants included in this file. 
-#' This value is usually provided when FamilySearch programs build a GEDCOM file for a patron requesting 
-#' a download of descendants. 
+#' @param gedcom_file_name The name of the GEDCOM file. A GEDCOM file name should use the format
+#' basename.ext, and use the file extension .GED (or .ged).
+#' @param gedcom_form A value that identifies the GEDCOM form used in this GEDCOM file. The value must be
+#' alphanumerical string. This string is case-sensitive. Only "LINEAGE-LINKED" is supported.
+#' @param gedcom_version_number The version number of the specification used.
+#' @param id_number A third-party number assigned to an individual. 
 #' @param language_of_text The human language in which the data in the file is normally read or 
 #' written. It is used primarily by programs to select language-specific sorting sequences and phonetic 
 #' name matching algorithms.
-#' @param language_preference The language in which a person prefers to communicate. Multiple language 
-#' preference is shown by using multiple occurrences in order of priority.
 #' @param multimedia_file_reference A complete local or remote file reference to the auxiliary data 
 #' to be linked to the GEDCOM context. Remote reference would include a network address where the multimedia 
 #' data may be obtained. 
@@ -106,7 +90,6 @@
 #' GEDCOM context. This allows processors to determine whether they can process the data object. 
 #' Any linked files should contain the data required, in the indicated format, to process the file data. 
 #' @param name_of_business Name of the business, corporation, or person that produced or commissioned the product.
-#' @param name_of_family_file Name under which family names for ordinances are stored in the temple's family file.
 #' @param name_of_product The name of the software product that produced this file.
 #' @param name_of_repository The official name of the archive in which the stated source material is stored.
 #' @param name_of_source_data The name of the electronic data source that was used to obtain the data in 
@@ -123,26 +106,23 @@
 #' @param name_piece_suffix Non-indexing name piece that appears after the given name and surname parts. 
 #' Different name suffix parts are separated by a comma.
 #' @param name_piece_surname Surname or family name. Different surnames are separated by a comma.
-#' @param name_piece_surname_prefix Surname prefix or article used in a family name. Different surname 
-#' articles are separated by a comma, for example in the name "de la Cruz", this value would be "de, la".
-#' @param name_romanized_variation A character vector giving romanized variations of the name. 
+#' @param name_piece_surname_prefix Surname prefix or article used in a family name. A surname prefix that 
+#' consists of multiple parts is written as is, and not modified in any way. Thus, the surname prefix for the 
+#' surname “de la Cruz” is “de la”.
+#' @param name_romanised_variation A character vector giving romanised variations of the name. 
 #' @param name_type Indicates the name type, for example the name issued or assumed as an immigrant.
-#' @param national_id_number A nationally-controlled number assigned to an individual. Commonly 
-#' known national numbers should be assigned their own tag, such as SSN for U.S. Social Security Number. 
-#' The use of the IDNO tag requires a subordinate TYPE tag to identify what kind of number is being stored.
 #' @param national_or_tribal_origin The person's division of national origin or other folk, house, kindred, 
 #' lineage, or tribal interest.
 #' @param nobility_type_title The title given to or used by a person, especially of royalty or other noble 
 #' class within a locality.
+#' @param number_of_relationships The number of different relationships (family groups) that this person was 
+#' known to have been a member of as a partner, regardless of whether the associated relationships are present
+#' in the GEDCOM file.
 #' @param occupation The kind of activity that an individual does for a job, profession, or principal activity.
-#' @param ordinance_process_flag A flag that indicates whether submission should be processed for clearing 
-#' temple ordinances. 
 #' @param pedigree_linkage_type A code used to indicate the child to family relationship for pedigree navigation 
 #' purposes.
-#' @param permanent_record_file_number The record number that uniquely identifies this record within a 
-#' registered network resource.
 #' @param phone_number A phone number.
-#' @param phonetic_type Indicates the method used in transforming the text to the phonetic variation.
+#' @param phonetisation_method Indicates the method used in transforming the text to the phonetic variation.
 #' @param physical_description An unstructured list of the attributes that describe the physical 
 #' characteristics of a person, place, or object. Commas separate each attribute.
 #' @param place_latitude The value specifying the latitudinal coordinate of the place name. 
@@ -155,12 +135,12 @@
 #' @param place_name The jurisdictional name of the place where the event took place. 
 #' Jurisdictions are separated by commas.
 #' @param place_phonetic_variation A character vector of phonetic variations of the place name.
-#' @param place_romanized_variation A character vector of romanized variations of the place name. 
+#' @param place_romanised_variation A character vector of romanised variations of the place name. 
 #' @param possessions A list of possessions (real estate or other property) belonging to this individual.
-#' @param product_version_number An identifier that represents the version level assigned to the system. 
+#' @param product_version_number The version of the product that created the GEDCOM file. 
 #' It is defined and changed by the creators of the product.
 #' @param publication_date_source_data A date_exact() object giving the date this source was published or created.
-#' @param receiving_system_name The name of the system expected to process the GEDCOM-compatible file.
+#' @param receiving_system_name The name of the system expected to process the GEDCOM file.
 #' @param relation_is_descriptor A word or phrase that states object 1's relation is object 2. 
 #' @param religious_affiliation A name of the religion with which this person, event, or record was affiliated.
 #' @param responsible_agency The organization, institution, corporation, person, or other entity that has 
@@ -171,10 +151,9 @@
 #' This should be the same word or phrase, and in the same language, that the recorder used to define 
 #' the role in the actual record. 
 #' @param role_in_event Indicates what role this person played in the event that is being cited in this context. 
-#' @param romanized_type Indicates the method used in transforming the text to a romanized variation.
+#' @param romanisation_method Indicates the method used in transforming the text to a romanised variation.
 #' @param scholastic_achievement A description of a scholastic or educational achievement or pursuit.
 #' @param sex_value A code that indicates the sex of the individual. 
-#' @param social_security_number A number assigned to a person in the United States for identification purposes. 
 #' @param source_call_number An identification or reference description used to file and retrieve items 
 #' from the holdings of a repository. 
 #' @param source_descriptive_title The title of the work, record, or item and, when appropriate, 
@@ -183,8 +162,8 @@
 #' and retrieving source records.
 #' @param source_jurisdiction_place The name of the lowest jurisdiction that encompasses all lower-level places 
 #' named in this source. 
-#' @param source_media_type A code, selected from one of the media classifications choices above, 
-#' that indicates the type of material in which the referenced source is stored.
+#' @param source_media_type A media classification code that indicates the type of material in which 
+#' the referenced source is stored.
 #' @param source_originator The person, agency, or entity who created the record. For a published work, 
 #' this could be the author, compiler, transcriber, abstractor, or editor. For an unpublished source, 
 #' this may be an individual, a government agency, church organization, or private organization, etc. 
@@ -192,28 +171,23 @@
 #' @param submitter_name The name of the submitter formatted for display and address generation. 
 #' @param submitter_registered_rfn A registered number of a submitter of Ancestral File data. 
 #' This number is used in subsequent submissions or inquiries by the submitter for identification purposes.
-#' @param system_id A system identification name which was obtained through the GEDCOM 
-#' registration process. This name must be unique from any other product. Spaces within the name 
-#' must be substituted with an underscore.
-#' @param temple_code An abbreviation of the temple in which LDS temple ordinances were performed.
+#' @param system_id A system identification name. This name must be unique for each system (product), 
+#' different from any other system. The name may include spaces, and is not restricted to ASCII characters.
 #' @param text_from_source A verbatim copy of any description contained within the source. 
 #' This indicates notes or text that are actually contained in the source document, not the submitter's 
 #' opinion about the source.  
 #' @param time_value,file_creation_time The time of a specific event. 
 #' @param user_reference_number A user-defined number or text that the submitter uses to identify this record. 
 #' @param user_reference_type A user-defined definition of the user_reference_number.
-#' @param user_text Comments or opinions from the submitter. 
-#' @param character_set_version_number An identifier that represents the version level assigned to the character set. 
-#' It is defined and changed by the creators of the product. 
+#' @param user_text Free-form user text. Comments, opinions. 
 #' @param where_within_source Specific location with in the information referenced. 
-#' @param xref_fam An xref ID of a family record.  
-#' @param xref_indi An xref ID of an individual record. 
-#' @param xref_sour An xref ID of a source record. 
-#' @param xref_repo An xref ID of a repository record. 
+#' @param xref_fam An xref ID of a family group record.
+#' @param xref_indi An xref ID of an individual record.
+#' @param xref_sour An xref ID of a source record.
+#' @param xref_repo An xref ID of a repository record.
 #' @param xref_obje An xref ID of a multimedia record.
 #' @param xref_note An xref ID of a note record. 
 #' @param xref_subm An xref ID of a submitter record.
-#' @param xref_subn An xref ID of a submission record.
 #' @param date_changed A CHANGE_DATE() object giving the time this record was last modified. If not provided,
 #' the current date is used.
 #' @param notes A list of NOTE_STRUCTURE() objects.

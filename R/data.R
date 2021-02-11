28d2ea04591241ff941894a3023db2d16ed4accc
Jamie Lendrum <jalendrum@gmail.com>
2021-02-11 21:13:42 +0000

    feat: add sample dataset

---
+++ R/data.R
@@ -0,0 +1,14 @@
+
+#' A sample GEDCOM file in tidyged format
+#'
+#' This is the official GEDCOM 5.5.5 sample file.
+#'
+#' @format A tibble with 97 rows and 4 variables:
+#' \describe{
+#'   \item{level}{The level in the GEDCOM structural hierarchy}
+#'   \item{record}{The unique cross reference for the record}
+#'   \item{tag}{The tag describing the nature of the line value given}
+#'   \item{value}{The GEDCOM line value}
+#' }
+#' @source \url{https://www.gedcom.org/samples.html}
+"sample555"

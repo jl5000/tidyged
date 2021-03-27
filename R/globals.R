2132c4ff7c1aba4223b7351799ce4bf7a2f83bda
Jamie Lendrum <jalendrum@gmail.com>
2021-03-27 21:23:29 +0000

    fix: global variable for R CMD check

--- R/globals.R
+++ R/globals.R
@@ -1,7 +1,7 @@
 # Stop the NOTES from R CMD CHECK when creating columns with mutate()
 if(getRversion() >= "2.15.1")  
   utils::globalVariables(c("level", "record", "tag", "value", ".", "num_children", "xref",
-                           "mother_xref", "father_xref", "num_siblings", "full"))
+                           "mother_xref", "father_xref", "num_siblings", "full", "n"))
 
 
 .pkgenv <- new.env(parent=emptyenv())
